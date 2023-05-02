1be7d55297672a10c92b3170ea6eb0585733d3dc
Alex_Jesús <alextaraguay20@gmail.com>
2023-05-02 07:35:27 -0500

    pagian web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { E404Component } from './component/e404/e404.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
 
-const routes: Routes = [];
+import { ProductosComponent } from './component/productos/productos.component';
+import { NosotrosComponent } from './component/nosotros/nosotros.component';
+
+const routes: Routes = [
+{path: '', redirectTo: 'home', pathMatch: 'full' },
+{path: 'home', component: HomeComponent},
+{path: 'contacto',component: ContactoComponent},
+{path: 'productos', component: ProductosComponent},
+{path: 'nosotros', component: NosotrosComponent},
+{path: '**', component: E404Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+
+import { E404Component } from './component/e404/e404.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
+
+import { FooterComponent } from './component/footer/footer.component';
+import { ProductosComponent } from './component/productos/productos.component';
+import { NosotrosComponent } from './component/nosotros/nosotros.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    E404Component,
+    NavbarComponent,
+    ContactoComponent,
+
+    FooterComponent,
+    ProductosComponent,
+    NosotrosComponent,
+  
   ],
   imports: [
     BrowserModule,
